fdc3dedeeab6419bb1c3606e09e46a6de687574a
Julia Ellboeck <juliaellboeck@gmail.com>
2019-11-03 12:46:29 -0500

    added images and built out HTML

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+var friendArray = [
+    {
+        name: "Riley",
+        photo:"/app/assets/images/riley.jpg",
+        scores:[
+
+        ]
+
+    },{
+        name: "Chloe",
+        photo:"/app/assets/images/chloe.jpg",
+        scores:[
+            
+        ]
+    },{
+        name: "Jack",
+        photo:"/app/assets/images/jack.jpg",
+        scores:[
+            
+        ]
+    },{
+        name: "Maggie",
+        photo:"/app/assets/images/maggie.jpg",
+        scores:[
+            
+        ]
+    },{
+        name: "Toby",
+        photo:"/app/assets/images/toby.jpg",
+        scores:[
+            
+        ]
+    },{
+        name: "Finn",
+        photo:"/app/assets/images/finn.jpg",
+        scores:[
+            
+        ]
+    },{
+        name: "Molly",
+        photo:"/app/assets/images/molly.jpg",
+        scores:[
+            
+        ]
+    },{
+        name: "Cody",
+        photo:"/app/assets/images/cody.jpg",
+        scores:[
+            
+        ]
+    }
+
+
+
+];
+
+module.exports = friendArray;
