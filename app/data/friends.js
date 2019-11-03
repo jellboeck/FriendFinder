cb954e294e6f7c14b626d689769e11660b5d31bd
Julia Ellboeck <juliaellboeck@gmail.com>
2019-11-03 13:57:55 -0500

    finished HTML

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,6 +3,16 @@ var friendArray = [
         name: "Riley",
         photo:"/app/assets/images/riley.jpg",
         scores:[
+            4,
+            4,
+            4,
+            3,
+            4,
+            5,
+            1,
+            4,
+            3,
+            3
 
         ]
 
@@ -10,48 +20,110 @@ var friendArray = [
         name: "Chloe",
         photo:"/app/assets/images/chloe.jpg",
         scores:[
-            
+            1,
+            1,
+            3,
+            1,
+            1,
+            3,
+            2,
+            1,
+            2,
+            5,
         ]
     },{
         name: "Jack",
         photo:"/app/assets/images/jack.jpg",
         scores:[
-            
+            5,
+            4,
+            1,
+            5,
+            5,
+            5,
+            3,
+            5,
+            1,
+            2,
+
         ]
     },{
         name: "Maggie",
         photo:"/app/assets/images/maggie.jpg",
         scores:[
-            
+            3,
+            1,
+            1,
+            2,
+            2,
+            5,
+            5,
+            2,
+            5,
+            4,
         ]
     },{
         name: "Toby",
         photo:"/app/assets/images/toby.jpg",
         scores:[
-            
+            4,
+            5,
+            2,
+            4,
+            4,
+            5,
+            3,
+            5,
+            2,
+            4,
         ]
     },{
         name: "Finn",
         photo:"/app/assets/images/finn.jpg",
         scores:[
-            
+            2,
+            2,
+            3,
+            4,
+            4,
+            3,
+            3,
+            5,
+            3,
+            3,
         ]
     },{
         name: "Molly",
         photo:"/app/assets/images/molly.jpg",
         scores:[
+            2,
+            1,
+            2,
+            2,
+            2,
+            3,
+            4,
+            2,
+            4,
+            3,
             
         ]
     },{
         name: "Cody",
         photo:"/app/assets/images/cody.jpg",
         scores:[
-            
+            5,
+            5,
+            1,
+            5,
+            4,
+            5,
+            3,
+            5,
+            5,
+            4
         ]
     }
-
-
-
 ];
 
 module.exports = friendArray;
