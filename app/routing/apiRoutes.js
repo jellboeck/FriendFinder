81fe29c3b2fb4ea3ad00ceee37ee627c167b663f
Julia Ellboeck <juliaellboeck@gmail.com>
2019-11-08 21:17:08 -0500

    App completed

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,39 +1,40 @@
-var friendList = require("../data/friends");
+var friends = require("../data/friends");
 
 module.exports = function(app) {
     
     app.get("/api/friends" , function (req, res){
-        res.json(friendList);
+        res.json(friends);
     })
 
-    app.post("/api/freinds" , function (req,res){
+    app.post("/api/friends" , function (req,res){
         
         var friendMatch = {
             name: "",
             photo: "",
             friendDiff: 1000
         }
-
+        console.log(req.body);
+        
         var userData = req.body;
         var userScores = userData.scores;
         var totalDifference= 0;
 
-        for (var i = 0; i < friendArray.length; i++){
+        for (var i = 0; i < friends.length; i++){
             totalDifference = 0;
 
-            for (var j = 0; j < friendArray[i].scores[j] ; j++){
-                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendArray[i].scores[j]))
+            for (var j = 0; j < friends[i].scores[j] ; j++){
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]))
 
                 if (totalDifference <= friendMatch.friendDiff) {
 
-                    friendMatch.name = friendArray[i].name;
-                    friendMatch.photo = friendArray[i].photo;
+                    friendMatch.name = friends[i].name;
+                    friendMatch.photo = friends[i].photo;
                     friendMatch.friendDiff = totalDifference;
                 }
             }
         }
-
-        friendArray.push(userData);
+        console.log(friendMatch);
+        friends.push(userData);
         res.json(friendMatch);
     })
 }
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+app.use(express.static("images"));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
